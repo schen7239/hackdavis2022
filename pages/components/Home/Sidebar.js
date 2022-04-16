b8a0aad927dd6734d9d86770fcb3f285045a8e89
schen7239 <schen7239@gmail.com>
2022-04-16 14:47:57 -0700

    sidebar fixed

--- pages/components/Home/Sidebar.js
+++ pages/components/Home/Sidebar.js
@@ -6,29 +6,29 @@ import { Logo } from '../../../public/svg/index';
 function Sidebar() {
   const router = useRouter();
   return (
-    <div className=" col-span-1 flex flex-col text-center bg-sidebar-gray">
+    <div className=" col-span-1 flex flex-col text-center bg-primary-blue">
       <div className="pt-10 pb-12 ml-[30%]" >
         <Logo />
       </div>
       <div className="flex flex-col align-items flex-1 text-gray-500 space-y-2">
         <div
-          className="flex items-center justify-center space-x-4 hover:bg-hover-gray p-3"
+          className="flex items-center justify-center space-x-4 hover:bg-[#1971D0] p-3"
           onClick={() => router.push("/")}
         >
-          <Icon icon="ant-design:home-outlined"  height={24} width={24} />
+          <Icon icon="ant-design:home-outlined"  height={24} width={24} color="white" />
         </div>
         <div
           onClick={() => router.push("/careers")}
-          className="flex items-center justify-center space-x-4 hover:bg-hover-gray p-3"
+          className="flex items-center justify-center space-x-4 hover:bg-[#1971D0] p-3"
         >
-          <Icon icon="ic:outline-business-center"  height={24} width={24} />
+          <Icon icon="ic:outline-business-center" color="white"  height={24} width={24} />
         </div>
-        <div className="flex items-center justify-center space-x-4 hover:bg-hover-gray p-3">
-          <Icon icon="cil:people" height={24} width={24} />
+        <div className="flex items-center justify-center space-x-4 hover:bg-[#1971D0] p-3">
+          <Icon icon="cil:people" height={24} width={24} color="white"  />
         </div>
       </div>
-      <div className="flex items-center justify-center space-x-4 mb-5 text-gray-500 hover:bg-hover-gray p-3">
-        <Icon icon="simple-line-icons:logout" height={24} width={24} />
+      <div className="flex items-center justify-center space-x-4 mb-5 text-gray-500 hover:bg-[#1971D0] p-3">
+        <Icon icon="simple-line-icons:logout" height={24} width={24} color="white"  />
       </div>
     </div>
   );
