a141b49b6f0239595beed409a6359ca33e70f544
canewton <cnewt2003@gmail.com>
2022-04-16 14:48:48 -0700

    building out careers page

--- pages/careers.js
+++ pages/careers.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Sidebar, SearchBar } from "./components";
+import Internships from "./data/internships.json";
+import Recommendations from "./data/reccomended.json";
+import { UserJobCard } from "./components/index";
 
 function Careers() {
   const OppuritunitiesTab = "opputitunities tab";
@@ -7,10 +10,61 @@ function Careers() {
   const ExercisesTab = "exercises tab";
   const [tab, setTab] = useState(OppuritunitiesTab);
 
+  function Reccomendation({ name, location }) {
+    return (
+      <div className="mb-5 border-b-2 border-black">
+        <p className="">{name}</p>
+        <p>{location}</p>
+      </div>
+    );
+  }
+
   function DisplayTab() {
     switch (tab) {
       case OppuritunitiesTab:
-        return <SearchBar />;
+        return (
+          <div className="grid grid-cols-2 bg-gray-100 mx-20 mt-20 gap-8">
+            <div>
+              <SearchBar />
+              <div className="mt-10">
+                {Internships?.map(
+                  ({ title, posted, description, isInternship, isInPerson }) => (
+                    <UserJobCard
+                      key={title}
+                      title={title}
+                      posted={posted}
+                      description={description}
+                      isInternship={isInternship}
+                      isInPerson={isInPerson}
+                    />
+                  )
+                )}
+              </div>
+              <div className="rounded-lg bg-white shadow-md my-5 flex flexRow p-10 content-center justify-between">
+                <div>
+                  <p className="ml-4 mt-1 text-3xl">Put your best foot forward</p>
+                  <p className="ml-4 mt-2">
+                    Request a resume review from an industry professional
+                  </p>
+                </div>
+                <button className="w-auto h-8 text-white bg-primary-blue rounded-full px-2 py-1 text-xs font-medium active:scale-90 transition duration-150 ease-out">
+                  Request
+                </button>
+              </div>
+            </div>
+            <div className="rounded-lg bg-white shadow-md p-10 ">
+              <div>
+                <p className="mt-1 text-2xl">Recomended for you</p>
+                <p className="mt-2">Based on your profile and search history</p>
+                <div className="mt-10">
+                  {Recommendations?.map(({ name, location }) => (
+                    <Reccomendation key={name} name={name} location={location} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
       case CheckInsTab:
         return <div>hi2</div>;
       case ExercisesTab:
@@ -19,10 +73,10 @@ function Careers() {
   }
 
   return (
-    <div className="h-[100vh] select-none">
-      <main className="h-[100%] flex flexRow grid grid-cols-5">
+    <div className="h-[100vh] select-none bg-main-bg-gray">
+      <main className="h-[100%] grid grid-cols-5">
         <Sidebar />
-        <div className="flex-1 col-span-4">
+        <div className="col-span-4">
           <div className="mx-40 mt-20 flex-1 columns-3">
             <p
               onClick={() => setTab(OppuritunitiesTab)}
--- pages/components/Home/SearchBar.js
+++ pages/components/Home/SearchBar.js
@@ -1,7 +1,25 @@
+import { Icon } from "@iconify/react";
 import React from "react";
 
 function SearchBar() {
-  return <div className="rounded bg-card-white w">blah</div>;
+  return (
+    <div className="rounded-full bg-white shadow-md flex flexRow p-2">
+      <div className="rounded-full bg-primary-blue py-3 px-5">
+        <Icon icon="codicon:search" color="white" />
+      </div>
+      <div className="columns-3 flex-1">
+        <div className="">
+          <p className="flex justify-center content-center">Field</p>
+        </div>
+        <div>
+          <p className="flex justify-center content-center">Location</p>
+        </div>
+        <div>
+          <p className="flex justify-center content-center">Term</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
