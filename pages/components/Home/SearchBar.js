b0d4ce96e3dc149ea259d53125da00cadb521ee7
schen7239 <schen7239@gmail.com>
2022-04-17 04:29:51 -0700

    fixed search

--- pages/components/Home/SearchBar.js
+++ pages/components/Home/SearchBar.js
@@ -1,12 +1,18 @@
 import { Icon } from "@iconify/react";
 import React, { useState } from "react";
 
-function SearchBar({ filteredJobsData, setFilteredJobsData }) {
+function SearchBar({ filteredJobsData, setFilteredJobsData, roleData, setRoleData  }) {
   const handleChange = (e) => {
     let newArray = [...filteredJobsData]
     let index = newArray.find(element => element == e.target.value);
     if (index == undefined) return setFilteredJobsData([...newArray, e.target.value])
     setFilteredJobsData([...newArray.filter(element => element != e.target.value)])
+  }
+  const handleRoleChange = (e) => {
+    let newArray = [...roleData]
+    let index = newArray.find(element => element == e.target.value);
+    if (index == undefined) return setRoleData([...newArray, e.target.value])
+    setRoleData([...newArray.filter(element => element != e.target.value)])
   }
   return (
     <div className="rounded-full bg-white shadow-md items-center flex flexRow p-2">
@@ -36,10 +42,12 @@ function SearchBar({ filteredJobsData, setFilteredJobsData }) {
             <input type="checkbox" onChange={handleChange} value="In-Person" />In-Person
             </label>
           </div>
-          {/* <div className="flex flex-col">
-            <input type="checkbox" />
-            <input type="checkbox" />
-          </div> */}
+          <div className="flex flex-col">
+            <h1>Roles</h1>
+            <label><input onClick={handleRoleChange} type="checkbox" value="develop"  />Software Developer</label>
+            <label><input onClick={handleRoleChange} type="checkbox" value="engineer" />Software Engineer</label>
+            <label><input onClick={handleRoleChange} type="checkbox" value="design" />UI/UX Designer</label>
+          </div>
         </div>
       </div>
     </div>
