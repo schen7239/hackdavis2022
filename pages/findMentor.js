167a8a0754f98c098406a5a1672faddf34762845
schen7239 <schen7239@gmail.com>
2022-04-16 20:34:27 -0700

    form done

--- pages/findMentor.js
+++ pages/findMentor.js
@@ -35,31 +35,44 @@ function FindMentor() {
   const [preferredTechField, setPreferredTechField] = useState("");
   const [softSkills, setSoftSkills] = useState("");
   const [mentorSkills, setMentorSkills] = useState("");
+  const [userWrittenResponse, setUserWrittenResponse] = useState('');
   return (
     <>
-      <div className="grid grid-cols-10 h-[100vh] w-full select-none">
+      <div className="grid grid-cols-10 h-auto w-full select-none bg-main-bg-gray">
         <Sidebar />
         <div className="col-span-9 p-10 w-full">
           <h1 className="font-medium text-xl">Hello Lauren!</h1>
           <h2 className="font-medium text-xl">
             Ready to get match? Answer a few short question.
           </h2>
-          <div className="f">
+          <div className="grid justify-center mt-10 space-y-4">
+            <div className="bg-white p-10 w-[800px] rounded-lg">
             <RadioButtonsGroup
               question="What is your preferred tech field?"
               inputArray={["UI/UX Design", "Web Developer", "Data Science"]}
               setFunction={setPreferredTechField}
             />
+            </div>
+            <div className="bg-white p-10 w-[800px] rounded-lg">
             <RadioButtonsGroup
               question="What soft skills do you want to gain?"
               inputArray={["Problem Solving", "Communicating", "Leadership"]}
               setFunction={setSoftSkills}
             />
+            </div>
+            <div className="bg-white p-10 w-[800px] rounded-lg">
             <RadioButtonsGroup
               question="What are you looking for in a mentor?"
               inputArray={["Empathy", "Guidance", "Attention to Detail"]}
               setFunction={setMentorSkills}
             />
+            </div>
+            <div className="bg-white p-10 w-[800px] rounded-lg space-y-2">
+              <h1 className="font-semibold">What do you like to do for fun?</h1>
+              <div className="border b-2 rounded-xl px-3 py-1">
+              <input className="outline-none" onChange={(e) => setUserWrittenResponse(e.target.value)} value={userWrittenResponse} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
