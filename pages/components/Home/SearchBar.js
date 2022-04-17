3f64d4e45afe94d8a90584f89ea1c8df1df697bf
schen7239 <schen7239@gmail.com>
2022-04-17 04:39:56 -0700

    filtering done

--- pages/components/Home/SearchBar.js
+++ pages/components/Home/SearchBar.js
@@ -1,56 +1,94 @@
 import { Icon } from "@iconify/react";
 import React, { useState } from "react";
 
-function SearchBar({ filteredJobsData, setFilteredJobsData, roleData, setRoleData  }) {
+function SearchBar({
+  filteredJobsData,
+  setFilteredJobsData,
+  roleData,
+  setRoleData,
+}) {
   const handleChange = (e) => {
-    let newArray = [...filteredJobsData]
-    let index = newArray.find(element => element == e.target.value);
-    if (index == undefined) return setFilteredJobsData([...newArray, e.target.value])
-    setFilteredJobsData([...newArray.filter(element => element != e.target.value)])
-  }
+    let newArray = [...filteredJobsData];
+    let index = newArray.find((element) => element == e.target.value);
+    if (index == undefined)
+      return setFilteredJobsData([...newArray, e.target.value]);
+    setFilteredJobsData([
+      ...newArray.filter((element) => element != e.target.value),
+    ]);
+  };
   const handleRoleChange = (e) => {
-    let newArray = [...roleData]
-    let index = newArray.find(element => element == e.target.value);
-    if (index == undefined) return setRoleData([...newArray, e.target.value])
-    setRoleData([...newArray.filter(element => element != e.target.value)])
-  }
+    let newArray = [...roleData];
+    let index = newArray.find((element) => element == e.target.value);
+    if (index == undefined) return setRoleData([...newArray, e.target.value]);
+    setRoleData([...newArray.filter((element) => element != e.target.value)]);
+  };
   return (
-    <div className="rounded-full bg-white shadow-md items-center flex flexRow p-2">
-      <div className="rounded-full bg-primary-blue py-3 px-5">
-        <Icon icon="codicon:search" color="white" />
-      </div>
-      <div  className="flex flex-col w-full">
-        <div className="flex justify-around w-full">
-          <div>
-            <p className="flex justify-center content-center">Field</p>
-          </div>
-          <div>
-            <p className="flex justify-center content-center">Location</p>
-          </div>
-          <div>
-            <p className="flex justify-center content-center">Term</p>
-          </div>
+    <>
+      <div className="rounded-full bg-white shadow-md items-center flex flexRow p-2">
+        <div className="rounded-full bg-primary-blue py-3 px-5">
+          <Icon icon="codicon:search" color="white" />
         </div>
-        <div className="flex">
-          <div className="flex flex-col">
-            <h1>Remote/In-Person</h1>
-            <label>
-            <input type="checkbox" onChange={handleChange} value="Remote" />
-            Remote
-            </label>
-            <label>
-            <input type="checkbox" onChange={handleChange} value="In-Person" />In-Person
-            </label>
-          </div>
-          <div className="flex flex-col">
-            <h1>Roles</h1>
-            <label><input onClick={handleRoleChange} type="checkbox" value="develop"  />Software Developer</label>
-            <label><input onClick={handleRoleChange} type="checkbox" value="engineer" />Software Engineer</label>
-            <label><input onClick={handleRoleChange} type="checkbox" value="design" />UI/UX Designer</label>
+        <div className="flex flex-col w-full">
+          <div className="flex justify-around w-full">
+            <div>
+              <p className="flex justify-center content-center">Field</p>
+            </div>
+            <div>
+              <p className="flex justify-center content-center">Location</p>
+            </div>
+            <div>
+              <p className="flex justify-center content-center">Term</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="bg-white rounded-lg mt-5 p-4 shadow-md">
+        <div className="flex justify-around">
+          <div className="flex flex-col">
+            <h1 className="text-sm text-primary-blue">Remote/In-Person</h1>
+            <label>
+              <input type="checkbox" onChange={handleChange} value="Remote" />
+              Remote
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                onChange={handleChange}
+                value="In-Person"
+              />
+              In-Person
+            </label>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-sm text-primary-blue">Roles</h1>
+            <label>
+              <input
+                onClick={handleRoleChange}
+                type="checkbox"
+                value="develop"
+              />
+              Software Developer
+            </label>
+            <label>
+              <input
+                onClick={handleRoleChange}
+                type="checkbox"
+                value="engineer"
+              />
+              Software Engineer
+            </label>
+            <label>
+              <input
+                onClick={handleRoleChange}
+                type="checkbox"
+                value="design"
+              />
+              UI/UX Designer
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
